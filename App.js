6f0c4b487bee72fbc7acc978cfce48afd59f48c2
Aniket Kumar <prince809263@gmail.com>
2023-10-31 15:54:49 +0530

    Episode-04

--- App.js
+++ App.js
@@ -1,16 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { restaurantList } from "./Dummy_data";
 
-// const parent = React.createElement("div", { id: "parent" }, "hello");
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/PqkxPzdIGHYlwJzMzFSLbWCaI0g=/0x0:1181x1181/500x500/top/smart/99designs-contests-attachments/134/134197/attachment_134197839"
+          alt="A Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+// Here we gave a props to a functional component
+const ReasturentCart = (props) => {
+  console.log(props);
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    area,
+    lastMileTravelString,
+    costForTwoString,
+    avgRating,
+  } = props;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      />
+      <h2>{name}</h2>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{area}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{lastMileTravelString}</h4>
+      <h4>{costForTwoString}</h4>
+    </div>
+  );
+};
 
-const head = <h1>hello2</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* Restaurent Card */}
+        {restaurantList.map((restaurant) => {
+          return (
+            <ReasturentCart key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <>
-    {head}
-    <h1>hello</h1>
-  </>
-);
+const AppLayout = () => {
+  return (
+    <div className="class">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
