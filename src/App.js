1d94dee2135422f5085079c80d424ed413887192
Aniket Kumar <prince809263@gmail.com>
2023-11-05 12:49:11 +0530

    use lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+// import Grocery from "./components/Grocery";
 import { RestaurentMenu } from "./components/RestaurentMenu";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 
-// Here we gave a props to a functional component
+// Chunking
+// code splitting
+// dynamic bundling
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
   return (
@@ -35,6 +40,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/Grocery",
+        element: (
+          <Suspense fallback={<h1>Loading .....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurents/:resId",
